8cbdfc6203a40b0627f9544b3dea8d60ed6d6f3c
R0rt1z2 <me@r0rt1z2.com>
2024-04-11 21:54:22 +0200

    stanford: Patch hwcam to not connect to Huawei sensors/perf HALs
    Change-Id: I236d93cee1af64dc1b77baa1eaa93a0d115d2c2b

--- extract-files.sh
+++ extract-files.sh
@@ -16,6 +16,18 @@ function blob_fixup() {
         odm/lib64/hwcam/hwcam.hi3660.m.STF.so)
             "${PATCHELF}" --remove-needed "vendor.huawei.hardware.ai@1.0.so" "${2}"
             "${PATCHELF}" --remove-needed "vendor.huawei.hardware.biometrics.hwsecurefacerecognize@1.0.so" "${2}"
+            ## NOP vendor.huawei.hardware.perfgenius calls
+            # CameraPerfImpl::connectLocked
+            "${SIGSCAN}" -p "e4 28 ef 97 fc 6f ba a9 fa 67 01 a9" -P "e4 28 ef 97 00 00 80 d2 c0 03 5f d6" -f "${2}"
+            ## NOP vendor.huawei.hardware.sensors calls
+            # TofSensorImpl::connectTofServiceLocked
+            "${SIGSCAN}" -p "4c d9 ee 97 fc 0f 1b f8 f8 5f 01 a9" -P "4c d9 ee 97 00 00 80 d2 c0 03 5f d6" -f "${2}"
+            # AwbSensorImpl::connectAwbServiceLocked
+            "${SIGSCAN}" -p "0d c3 ed 97 fc 0f 1b f8 f8 5f 01 a9" -P "0d c3 ed 97 00 00 80 d2 c0 03 5f d6" -f "${2}"
+            # FlickerSensorImpl::connectFlickerServiceLocked
+            "${SIGSCAN}" -p "06 bb ed 97 fc 0f 1b f8 f8 5f 01 a9" -P "06 bb ed 97 00 00 80 d2 c0 03 5f d6" -f "${2}"
+            # LightSensorImpl::connectLightServiceLocked
+            "${SIGSCAN}" -p "68 b5 ed 97 fc 0f 1b f8 f8 5f 01 a9" -P "68 b5 ed 97 00 00 80 d2 c0 03 5f d6" -f "${2}"
             ;;
     esac
 }
