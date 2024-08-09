ab2f57420f7a085d124c075d9f29d4ea3b03daa8
Coconut <coconutat@protonmail.com>
2024-08-09 11:09:18 +0800

    victoria: Set right Partitions size
    Huawei P10 (VTR-AL00) doesn't have prets adn pretvs partition.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,20 @@ DEVICE_PATH := device/huawei/victoria
 # Inherit from hi3660-common
 include device/huawei/hi3660-common/BoardConfigCommon.mk
 
+# Kernel
+TARGET_KERNEL_CONFIG := merge_hi3660_P10_defconfig
+
+# Partitions
+# BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
+
+# Partitions - dynamic
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := cust eng_system eng_vendor odm patch preas preavs product system vendor
+BOARD_SUPER_PARTITION_PREAS_DEVICE_SIZE := 310378496 # /dev/block/mmcblk0p60
+BOARD_SUPER_PARTITION_PRODUCT_DEVICE_SIZE := 511705088 # /dev/block/mmcblk0p57
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 2860515328 # /dev/block/mmcblk0p59
+
+BOARD_SUPER_PARTITION_SIZE := 5469372416 # The sum of the total sizes of all partitions
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
